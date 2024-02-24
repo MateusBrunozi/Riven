a029a63894e461fd2e6a35209d07f2ee75fafa77
Mateus Brunozi <mateusbrunozi@hotmail.com>
2024-02-23 22:55:15 -0300

    SiteRiven

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const button = document.querySelector('button')
 const popup = document.querySelector('.popup-wrapper')
+const body = document.querySelector('body')
 
-
-button.addEventListener('click',() => {
-    popup.style.display ='block'
+button.addEventListener('click', () => {
+    popup.style.display = 'block'
+    body.classList.add('no-scroll')
 })
-
 
 popup.addEventListener('click', event => {
     const classNameOfClickedElement = event.target.classList[0]
 
-    if(classNameOfClickedElement === 'popup-close' || classNameOfClickedElement ==='popuplink' || classNameOfClickedElement === 'popup-wrapper'){
+    if(classNameOfClickedElement === 'popup-close' || classNameOfClickedElement === 'popuplink' || classNameOfClickedElement === 'popup-wrapper'){
         popup.style.display = 'none'
+        body.classList.remove('no-scroll')
     }
-    
 })
